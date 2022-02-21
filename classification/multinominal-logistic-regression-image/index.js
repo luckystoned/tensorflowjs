2002f162781f88d2f0cc6e2c63e7e70c2f36db19
Luckystoned <luckystoned@github.com>
2022-02-21 19:54:07 -0300

    image recognition In action

---
+++ classification/multinominal-logistic-regression-image/index.js
@@ -0,0 +1,36 @@
+require('@tensorflow/tfjs')
+const tf = require('@tensorflow/tfjs')
+const LogisticRegression = require('./logistic-regression')
+const plot = require('node-remote-plot')
+const _ = require('lodash')
+const mnist = require('mnist-data')
+
+const mnistData = mnist.training(0, 1000)
+
+const features = mnistData.images.values.map(imag => _.flatMap(imag))
+
+const encodedLabels = mnistData.labels.values.map(label => {
+    const row = new Array(10).fill(0)
+    row[label] = 1
+    return row
+})
+
+const regression = new LogisticRegression(features, encodedLabels, {
+    learningRate: 1,
+    iterations: 20,
+    batchSize: 100
+})
+
+regression.train()
+
+const testMnistData = mnist.testing(0, 100)
+const testFeatures = testMnistData.images.values.map(imag => _.flatMap(imag))
+const testEcodedLabels = testMnistData.labels.values.map(label => {
+    const row = new Array(10).fill(0)
+    row[label] = 1
+    return row
+})
+
+const accuracy = regression.test(testFeatures, testEcodedLabels)
+console.log("Accuracy: ", accuracy)
+
