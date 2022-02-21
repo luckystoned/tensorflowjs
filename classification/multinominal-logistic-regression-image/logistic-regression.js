dbf9ac7076b604d026276537da12f01a45c70f57
Luckystoned <luckystoned@github.com>
2022-02-21 20:38:58 -0300

    optimization

--- classification/multinominal-logistic-regression-image/logistic-regression.js
+++ classification/multinominal-logistic-regression-image/logistic-regression.js
@@ -26,7 +26,7 @@ class LogisticRegression {
       .matMul(differences)
       .divNoNan(this.features.shape[0]);
 
-    this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
+    return this.weights.sub(slopes.mul(this.options.learningRate));
   }
 
   train() {
@@ -39,13 +39,15 @@ class LogisticRegression {
         const startIndex = j * this.options.batchSize;
         const { batchSize } = this.options;
 
-        const featureSlice = this.features.slice(
-          [startIndex, 0],
-          [batchSize, -1]
-        );
-        const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1]);
-
-        this.gradientDescent(featureSlice, labelSlice);
+        this.weights = tf.tidy(() => {
+          const featureSlice = this.features.slice(
+            [startIndex, 0],
+            [batchSize, -1]
+          );
+          const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1]);
+  
+          return this.gradientDescent(featureSlice, labelSlice);
+        })
       }
 
       this.recordCost();
