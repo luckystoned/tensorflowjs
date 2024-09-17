2b1798a64c36fbec21f7eb12edff715fe2c02da3
Luckystoned <luckystoned.dev@gmail.com>
2024-09-17 19:35:18 -0300

    Data preparation its done

--- linear-regression/index.js
+++ linear-regression/index.js
@@ -8,20 +8,65 @@ async function plot(pointsArray, featureName) {
     }
   );
 }
+
+function normalise(tensor) {
+  const min = tensor.min();
+  const max = tensor.max();
+  const normalisedTensor = tensor.sub(min).div(max.sub(min));
+  return {
+    tensor: normalisedTensor,
+    min: min,
+    max: max,
+  };
+}
+
+function denormalise(tensor, min, max) {
+  const denormalisedTensor = tensor.mul(max.sub(min)).add(min);
+  return denormalisedTensor;
+}
+
 async function run() {
+  // Import the dataset
   const housesSalesDataset = tf.data.csv(
     "http://127.0.0.1:5500/linear-regression/kc_house_data.csv"
   );
-  const sampleDataset = housesSalesDataset.take(10);
-  const dataAsArray = await sampleDataset.toArray();
-  console.log(dataAsArray);
 
-  const points = housesSalesDataset.map((record) => ({
+  // Extract the data
+  const pointsDataset = housesSalesDataset.map((record) => ({
     x: record.sqft_living,
     y: record.price,
   }));
+  const points = await pointsDataset.toArray();
+  if (points.length % 2 !== 0) {
+    // Remove the last element if the length is odd
+    points.pop();
+  }
+  tf.util.shuffle(points);
 
-  plot(await points.toArray(), "Saquare Feet");
+  plot(await points, "Saquare Feet");
+
+  // Extract Features (inputs)
+  const featureValues = await points.map((p) => p.x);
+  const featureTensor = tf.tensor2d(featureValues, [featureValues.length, 1]);
+
+  // Extract Labels (outputs)
+  const labelValues = await points.map((p) => p.y);
+  const labelTensor = tf.tensor2d(labelValues, [labelValues.length, 1]);
+
+  // Normalise the data
+  const normalisedFeature = normalise(featureTensor);
+  const normalisedLabel = normalise(labelTensor);
+
+  const [trainingFeatureTensor, testingFeatureTensor] = tf.split(
+    normalisedFeature.tensor,
+    2
+  );
+  const [trainingLabelTensor, testingLabelTensor] = tf.split(
+    normalisedLabel.tensor,
+    2
+  );
+
+  trainingFeatureTensor.print(true);
 }
 
 run();
